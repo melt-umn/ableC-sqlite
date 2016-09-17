ac06552b3b2390e98044a148f83dc896c1ca73d6
Eric Van Wyk <evw@cs.umn.edu>
2016-09-17 10:27:15 -0500

    more sqlite tweaks

---
+++ examples/README.md
@@ -0,0 +1,10 @@
+# To Do
+
++ create a `create_database.xc` example that creates the sample
+  database, this way test.db isn't in the repo.
++ rename examples to something more meaningful
++ rewrite this README to explain it all.
+
+### Minor
++ remove test.db
++ put sqlite.xh in the proper place
--- examples/compile.sh
+++ examples/compile.sh
@@ -6,7 +6,7 @@
 # Of course, if the use of 'cut' below fails for you, then just run
 # the commands individually by hand.
 
-java -jar ../artifact/ableC.jar $1
+java -jar ../artifact/ableC.jar $1 -I ../include
 
 # extract the base filename, everything before the dot (.)
 
@@ -16,9 +16,10 @@ filename_withoutpath=$(basename $filename)
 basefilename="${filename_withoutpath%.*}"
 
 cfile="${basefilename}.pp_out.c"
+#cfile="${basefilename}.c"
 
 # ToDo: check that sqlite.o exists, if not print
 # a message telling the users how to go build it.
 # It is as easy as `gcc -c sqlite3.c`
 
-gcc -lpthread -ldl ../sqlite/sqlite3.o ${cfile}
+gcc -lpthread -ldl ../sqlite/sqlite3.o ${cfile} -o ${basefilename}
