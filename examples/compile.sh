528e898fe554ff475e34a7d563954bd17fc90c3b
Travis Carlson <carl4980@umn.edu>
2016-09-18 14:50:44 -0500

    use -lsqlite3 if it is available

--- examples/compile.sh
+++ examples/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # This script shows the steps in compiling an extended C program (.xc)
 # down to C via an extended instance of ableC and then using GCC to
@@ -6,7 +7,9 @@
 # Of course, if the use of 'cut' below fails for you, then just run
 # the commands individually by hand.
 
-java -jar ../artifact/ableC.jar $1 -I ../include
+cmd="java -jar ../artifact/ableC.jar $1 -I ../include"
+echo $cmd
+$cmd
 
 # extract the base filename, everything before the dot (.)
 
@@ -18,6 +21,17 @@ basefilename="${filename_withoutpath%.*}"
 cfile="${basefilename}.pp_out.c"
 #cfile="${basefilename}.c"
 
-CC=gcc make ../sqlite/sqlite3.o
+if ldconfig -p | grep -q libsqlite3; then
+	link_sqlite3="-lsqlite3"
+elif [ -f ../sqlite/sqlite3.c ]; then
+	CC=gcc make ../sqlite/sqlite3.o
+	link_sqlite3="../sqlite/sqlite3.o"
+else
+	echo "ERROR: sqlite3 library not found" >&2
+	exit 255
+fi
 
-gcc -lpthread -ldl ../sqlite/sqlite3.o ${cfile} -o ${basefilename}
+cmd="gcc -lpthread -ldl ${link_sqlite3} ${cfile} -o ${basefilename}"
+echo $cmd
+$cmd
+
