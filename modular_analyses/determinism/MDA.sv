ed7de1c748b1f8cab544efe7788c4860632d7a22
Travis Carlson <carl4980@umn.edu>
2016-09-17 23:46:08 -0500

    add modular analyses (they're failing)

---
+++ modular_analyses/determinism/MDA.sv
@@ -0,0 +1,9 @@
+grammar edu:umn:cs:melt:exts:ableC:sqlite:modular_analyses:determinism;
+
+import edu:umn:cs:melt:ableC:host;
+
+copper_mda testStmt(ablecParser) {
+  edu:umn:cs:melt:ableC:host;
+  edu:umn:cs:melt:exts:ableC:sqlite;
+}
+
